70ab740635abde2f4edb6f8c73b5e561ffd5d740
cristhianGigpz <cjacevedot@gmail.com>
2026-06-10 10:55:48 -0500

    go avanzado Modulo 1 Base de datos Relacional conexion

--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
 	fmt.Println("Hello World, GO avanzado !")
+	conn, err := pgx.Connect(
+		context.Background(),
+		"postgres://postgres:gigpz@localhost:5434/bd_tests",
+	)
 
+	if err != nil {
+		panic(err)
+	}
+
+	sql := `
+	CREATE TABLE IF NOT EXISTS users2(
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100),
+		email VARCHAR(100)
+	)
+	`
+
+	_, err = conn.Exec(context.Background(), sql)
+	if err != nil {
+		panic(err)
+	}
+
+	defer conn.Close(context.Background())
+	fmt.Println("Conectado correctamente, tabla creada")
 }
